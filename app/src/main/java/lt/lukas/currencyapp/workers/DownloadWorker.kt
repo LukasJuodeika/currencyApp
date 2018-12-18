6a7918a3d99abb51151ac9a4758e7d025c9db2b2
Lukas Juodeika <lksjuodeika@gmail.com>
2018-12-18 11:09:07 +0200

    implement downloadWorker

--- app/src/main/java/lt/lukas/currencyapp/workers/DownloadWorker.kt
+++ app/src/main/java/lt/lukas/currencyapp/workers/DownloadWorker.kt
@@ -3,12 +3,21 @@ package lt.lukas.currencyapp.workers
 import android.content.Context
 import android.util.Log
 import androidx.work.*
+import lt.lukas.currencyapp.base.BaseApplication
+import lt.lukas.currencyapp.di.AppModule
+import lt.lukas.currencyapp.repository.MainRepository
+import org.koin.android.ext.android.getKoin
 
 class DownloadWorker(appContext: Context, workerParams: WorkerParameters) : Worker(appContext, workerParams)
 {
 
     override fun doWork(): Result {
         Log.d("WorkManager","periodic work")
+
+        val koin = (applicationContext as BaseApplication).getKoin()
+        val mainRepository = koin.get<MainRepository>()
+        mainRepository.parseNewData()
+
         return Result.success()
     }
 
